b23436a312caae15fa549c98524cbbea3f91bb7c
AnastasiaGer <aagermanova@gmail.com>
2021-06-16 17:29:31 -0700

    fix

--- DragDrop/script.js
+++ DragDrop/script.js
@@ -16,28 +16,25 @@ function dragstart (event) {
   setTimeout(() => {
     event.target.classList.add('hide'), 0
   })
- 
-
 }
 
 function dragend (event) {
   event.target.className = 'item'
 }
 
-
 function dragover (event) {
   event.preventDefault()
 }
+
 function dragenter (event) {
   event.target.classList.add('hovered')
-
 }
+
 function dragleave (event) {
   event.target.classList.remove('hovered')
-
 }
+
 function dragDrop (event) {
   event.target.classList.remove('hovered')
   event.target.append(item)
-
 }
